77a99334474915349633477690d8daa98520eaba
Maxim Tkachev <maxim.tkachev@in-aim.ru>
2019-07-30 13:20:58 +0300

    add cron tap for update cache data, add routes, fix controller

--- app/controllers/urls_controller.rb
+++ app/controllers/urls_controller.rb
@@ -1,35 +1,36 @@
 class UrlsController < ApplicationController
   def get_url
     cache = ActiveSupport::Cache::RedisCacheStore.new
-    ret_url = cache.read(params[:url])
+    ret_url = cache.read(params[:id])
     render json: { url: ret_url }, status: 200
   end
 
   def set_url
     long_url = params[:url]
     if check_url
-      short_url = Url.new(long_url: long_url, short_url: generate_url)
+      short_url = Url.new(long_url: long_url, short_url: SecureRandom.hex(3))
       if short_url.save
-        render json: { short_url: short_url.short_url }, status: 200
+        LoadUrlInRedisJob.perform_now
+        render json: { short_url: generate_url + short_url.short_url }, status: 200
       else
         render status: 400
       end
     else
-      render json: { short_url: find_sort_url }, status: 200
+      render json: { short_url: generate_url + find_short_url.short_url }, status: 200
     end
   end
 
   private
 
   def check_url
-    find_sort_url.nil?
+    find_short_url.nil?
   end
 
   def generate_url
-    ENV[site_url] + '/' + SecureRandom.hex(3)
+    ENV['site_url'] + '/'
   end
 
-  def find_sort_url
+  def find_short_url
     Url.find_by(long_url: params[:url])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   require 'sidekiq/web'
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
-
+  get 'set_url', to: 'urls#set_url'
+  get '/:id', to: 'urls#get_url'
 end
